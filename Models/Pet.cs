a376911b135a666c0965a0baa8b4b11f2d3acca1
Ben Capp <bentucker333@gmail.com>
2023-03-29 15:57:58 -0500

    created pet owner model

--- Models/Pet.cs
+++ Models/Pet.cs
@@ -5,8 +5,8 @@ using System;
 
 namespace pet_hotel
 {
-    public enum PetBreedType { }
-    public enum PetColorType { }
+    public enum PetBreedType { Shepherd, Poodle, Beagle, Bulldog, Terrier, Boxer, Labrador, Retriever }
+    public enum PetColorType { White, Black, Golden, Tricolor, Spotted }
     public class Pet
     {
         public int id { get; set; }
@@ -14,8 +14,17 @@ namespace pet_hotel
         [Required]
         public string name { get; set; }
 
-        // [Required]
-        // //breed, color, checked in
-        // public string 
+        [Required]
+        public PetBreedType breed { get; set; }
+
+        [Required]
+        public PetColorType color { get; set; }
+
+        public string? checkedInAt { get; set; }
+
+        [ForeignKey("petOwner")]
+        public int petOwnerid { get; set; }
+
+        public PetOwner petOwner { get; set; }
     }
 }
